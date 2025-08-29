e748da616ab08a07872f1e8e694af78d86d35e41
Aidr0ne <aidrobin2@gmail.com>
2025-08-29 20:20:12 +0100

    DAWN-8

--- BlueDawn.py
+++ BlueDawn.py
@@ -1,6 +1,17 @@
 import camera
 import Math
 
+from datetime import datetime, timedelta
+from time import sleep
+import tomllib
+
+with open("config.toml", "rb") as f:
+    config = tomllib.load(f)
+
 def main() -> int:
     """ Returns ISS Speed"""
+    start_time = datetime.now()
+    now_time = datetime.now()
+    while (now_time < start_time + timedelta(minutes=config["TimeRun"])):
+        now_time = datetime.now()
     return 0
